76fae370e0c9c32f5e8602a77b3f47ec18cbfc42
0-jerry <ldc1000@naver.com>
2026-07-21 15:32:05 +0900

    [Init] 프로젝트 생성

---
+++ App/GitItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitItApp.swift
+//  GitIt
+//
+//  Created by jerry on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct GitItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
