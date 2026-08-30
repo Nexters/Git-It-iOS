29473076a18d3ef9227e12059c71ee4f42c531e3
0-jerry <ldc1000@naver.com>
2026-08-30 15:23:51 +0900

    [Fix] 홈 화면 배경을 흰색으로 변경
    Refs: docs/spec-kit/018-home-screen/trouble-shooting.md

--- sources/Projects/Feature/Home/Screens/HomeScreen.swift
+++ sources/Projects/Feature/Home/Screens/HomeScreen.swift
@@ -19,7 +19,7 @@ public struct HomeScreen: View {
     @Bindable public var store: StoreOf<HomeFeature>
 
     public var body: some View {
-        ScreenContainer {
+        ScreenContainer(background: SemanticColorToken(name: "HomeScreenBackground", colorToken: .grey100)) {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
                     profileHeader
